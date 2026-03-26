e10e4a6790a9ff1f78ead8585e301dd24b17fd51
Nathan Shankar <nathanshankar465@gmail.com>
2026-03-26 15:37:42 +0000

    modified:   .github/workflows/ros_ci.yml 	modified:   gz_dynamic_projector/package.xml 	modified:   realsense_cam/launch/depth.launch.py

--- realsense_cam/launch/depth.launch.py
+++ realsense_cam/launch/depth.launch.py
@@ -7,40 +7,36 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
-
 def generate_launch_description():
     ld = LaunchDescription()
-    
 
     # Specify the name of the package and path to xacro file within the package
     pkg_name = 'realsense_cam'
     file_subpath = 'urdf/test_d455_camera.urdf.xacro'
-    
+
     gz_start_world = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([get_package_share_directory('ros_gz_sim'), '/launch', '/gz_sim.launch.py']),
         launch_arguments={
-            'gz_args' : '-r ' + '/home/nathan/clearpath_dev/src/gz_dynamic_projector/worlds/dynamic_projector_world.sdf'
+            'gz_args': '-r ' + '/home/nathan/clearpath_dev/src/gz_dynamic_projector/worlds/dynamic_projector_world.sdf'
             }.items(),
     )
 
     # Add features
     node_spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', '/robot_description',
-                                   '-z', '0.5'],
-                        output='screen')
-
+                             arguments=['-topic', '/robot_description',
+                                        '-z', '0.5'],
+                             output='screen')
 
     # Use xacro to process the file
-    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
     robot_description_raw = xacro.process_file(xacro_file).toxml()
-
 
     # robot state publisher node
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description_raw}] # add other parameters here if required
+        parameters=[{'robot_description': robot_description_raw}]
     )
 
     # joint state publisher (GUI) node
@@ -62,7 +58,7 @@ def generate_launch_description():
             '/infra1/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
             '/infra2/ir/image@sensor_msgs/msg/Image[gz.msgs.Image',
             '/infra2/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
-            '/depth/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
+            '/depth/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
         ],
         remappings=[
             ('/color/image_raw', '/camera/color/image_raw'),
@@ -73,10 +69,9 @@ def generate_launch_description():
             ('/infra1/camera_info', '/camera/infra1/camera_info'),
             ('/infra2/ir/image', '/camera/infra2/image_raw'),
             ('/infra2/camera_info', '/camera/infra2/camera_info'),
-            ('/depth/points', '/camera/depth/points'),
+            ('/depth/points', '/camera/depth/points')
         ]
     )
-    
 
     # Rviz node
     node_rviz = Node(
@@ -93,6 +88,6 @@ def generate_launch_description():
     ld.add_action(node_spawn_entity)
     ld.add_action(node_robot_state_publisher)
     ld.add_action(ros_gz_bridge)
-    #ld.add_action(node_joint_state_publisher)
+    # ld.add_action(node_joint_state_publisher)
     ld.add_action(node_rviz)
     return ld
